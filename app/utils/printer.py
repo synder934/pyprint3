d4c506177ce069fecd200209a636b3e7711b34f0
synder <110554156+synder934@users.noreply.github.com>
2025-04-17 19:20:26 +0100

    added printer class

--- app/utils/printer.py
+++ app/utils/printer.py
@@ -1,4 +1,35 @@
+import serial
+import time
 
 
 class Printer:
-    
+    def __init__(self, port: str = None, baudrate: int = 115200):
+        self.port = port if port else "/dev/ttyUSB0"
+        self.baudrate = baudrate
+        self.connection = None
+
+    def _connect(self):
+        try:
+            if not self.connection:
+                self.connection = serial.Serial(self.port, self.baudrate, timeout=1)
+                time.sleep(1)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def _sendCommand(self, command):
+        try:
+            self.connection.write(f"{command}\n".encode())
+            time.sleep(1)
+            res = self.connection.readlines()
+            print([line.decode().strip() for line in res])
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+
+if __name__ == "__main__":
+    printer = Printer()
+    printer._connect()

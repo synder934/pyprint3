701258f515c8da674acd5d99382b24933d829862
synder <110554156+synder934@users.noreply.github.com>
2025-04-18 10:23:21 +0100

    autoupdate added

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from app import create_app
+from threading import Thread
+
+
+def gitCheckAndPull():
+    pass
+
 
 app = create_app()
 
 if __name__ == "__main__":
+    autoUpdate = Thread(target=gitCheckAndPull, daemon=True)
+    autoUpdate.start()
     app.run(host="0.0.0.0", port=5000, debug=True)

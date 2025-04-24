b92a4a9815147d4598433bfc3da9c9ec5ae87758
synder <110554156+synder934@users.noreply.github.com>
2025-04-25 00:33:35 +0100

    changes

--- app/routes.py
+++ app/routes.py
@@ -20,11 +20,11 @@ current_app: customFlask
 
 @main.route("/", methods=["GET", "POST"])
 def index():
-    current_app.camera.refresh_source()
+    # current_app.camera.refresh_source()
     return render_template(
         "index.html",
         printer=current_app.printer,
-        camera=current_app.camera,
+        # camera=current_app.camera,
         os=os,
         sys=sys,
         socket=socket,

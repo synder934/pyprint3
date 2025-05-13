a827b56af2179392b487a1cfd5d6d38cc817c6ec
synder <tobyred8@hotmail.co.uk>
2025-05-13 23:46:20 +0100

    chcnges

--- app/utils/printJob.py
+++ app/utils/printJob.py
@@ -1,9 +1,6 @@
-
 class PrintJob:
-    def __init__(self, file_path:str):
+    def __init__(self, file_path: str):
         self.file_path = file_path
         self.__line_number = 0
         self.__paused = False
         self.__stopped = False
-        self.
-    
--- app/utils/printer.py
+++ app/utils/printer.py
@@ -9,72 +9,86 @@ from io import TextIOWrapper
 
 import serial.tools
 import serial.tools.list_ports
+
 from .logger import LogBook
 
 
-class Printer:
-    IDLE = 0
-    PAUSED = 1
-    PRINTING = 2
+IDLE = 0
+PAUSED = 1
+PRINTING = 2
 
+
+class Printer:
     def __init__(self, port: str = None, baudrate: int = 115200):
+        """Creates a new Printer object allowing the user to interface with the printer
+
+        Args:
+            port (str, optional): _description_. Defaults to None.
+            baudrate (int, optional): _description_. Defaults to 115200.
+        """
         self.port = port
         self.baudrate = baudrate
         self.__connection = None
-        self.__ready_to_recieve = True
+        self.__printer_is_busy = True
         self.__log: LogBook = LogBook()
         self.__command_queue: list[dict] = []
 
         self.__gcode_filepath = None
         self.__gcode_file: TextIOWrapper = None
-        self.__linecount = 0
-        self.__print_state = Printer.IDLE
+        self.__print_state: int = IDLE
 
         interface = threading.Thread(target=self.__interface, daemon=True)
         interface.start()
 
-    def __interface(self):
+    def __interface(self) -> None:
+        """interface loop to send and recieve commands with the printer"""
         while True:
-            try:  # add next print command to queue
-                if (
-                    self.__print_state == Printer.PRINTING
-                    and self.__gcode_file is not None
-                ):
-                    line = self.__gcode_file.readline().strip().split(";")[0]
-                    print(line)
-                    self.queue_command(line, author="PRINTFILE")
+            if self.__print_state == PRINTING and self.__gcode_file is not None:
+                self.__queue_next_gcode_command()
+            self.__send_next_queued_command()
+            self.__log_data_from_serial()
 
-            except Exception as e:
-                print(e)
+    def __log_data_from_serial(self) -> None:
+        """reads data from serial port if open and logs all commands"""
+        try:
+            if self.__connection is not None:
+                newLines = self.__connection.readlines()
+                for line in map(lambda x: x.decode().strip(), newLines):
+                    kwargs = line.split(":")
+                    if "busy" in kwargs:
+                        self.__printer_is_busy = True
+                    if "ok" in kwargs:
+                        self.__printer_is_busy = False
+                    self.add_log("PRINTER", line)
+        except Exception as e:
+            print(e)
 
-            try:  # send next command in queue if ready
-                if len(self.__command_queue) and self.__ready_to_recieve:
-                    command: str = self.__command_queue.pop(0)
-                    self.add_log("SERVER", command)
-                    if self.__connection is not None:
-                        self.__connection.write("{}\n".format(command).encode())
-                    else:
-                        self.add_log("SERVER", "printer is offline")
-            except Exception as e:
-                print(e)
-
-            try:  # read lines from serial connection
+    def __send_next_queued_command(self) -> None:
+        """sends next command in queue to printer"""
+        try:
+            if len(self.__command_queue) and not self.__printer_is_busy:
+                command: str = self.__command_queue.pop(0)
+                self.add_log("SERVER", command)
                 if self.__connection is not None:
-                    newLines = self.__connection.readlines()
-                    for line in map(lambda x: x.decode().strip(), newLines):
-                        kwargs = line.split(":")
-                        if "busy" in kwargs:
-                            self.__ready_to_recieve = False
-                        if "ok" in kwargs:
-                            self.__ready_to_recieve = True
-                        self.add_log("PRINTER", line)
-            except Exception as e:
-                print(e)
+                    self.__connection.write("{}\n".format(command).encode())
+                    self.__printer_is_busy = True
+                else:
+                    self.add_log("SERVER", "printer is offline")
+        except Exception as e:
+            print(e)
 
-    def connect(self):
+    def __queue_next_gcode_command(self) -> None:
+        """queue next gcode line from printing file"""
+        try:
+            line = self.__gcode_file.readline().strip().split(";")[0]
+            self.queue_command(line, author="PRINTFILE")
+        except Exception as e:
+            print(e)
+
+    def connect(self) -> bool:
+
         if self.port is None:
             return False
-
         try:
             if not self.__connection:
                 self.__connection = serial.Serial(self.port, self.baudrate, timeout=1)
@@ -82,21 +96,24 @@ class Printer:
         except Exception as e:
             return False
 
-    def disconnect(self):
+    def disconnect(self) -> None:
         self.__connection.close()
         self.__connection = None
         self.port = None
 
-    def get_ports(self):
+    def get_ports(self) -> list[str]:
         return [port.device for port in serial.tools.list_ports.comports()]
 
-    def set_port(self, port: str):
+    def get_port(self) -> str:
+        return self.port
+
+    def set_port(self, port: str) -> None:
         self.port = port
 
-    def get_log_text(self):
+    def get_log_text(self) -> str:
         return str(self.__log).splitlines()
 
-    def add_log(self, author: str, text: str):
+    def add_log(self, author: str, text: str) -> None:
         *kwargs, _ = text.split(":")
         level = 1
 
@@ -107,48 +124,40 @@ class Printer:
 
         self.__log.add_log(author, text, level)
 
-    def queue_command(self, command, author: str = "USER"):
+    def queue_command(self, command: str, author: str = "USER") -> None:
+        """appends command to command queue
+
+        Args:
+            command (str): command to be appended
+            author (str, optional): sender of command. Defaults to "USER".
+        """
         self.add_log(author, f"command queued: {command}")
         self.__command_queue.append(command)
 
-    def set_filename(self, filename: str):
+    def set_filename(self, filename: str) -> None:
         if self.__gcode_file is not None:
             self.__gcode_file.close()
         self.__gcode_filepath = os.path.join(os.curdir, "uploads", filename)
         self.__gcode_file = open(self.__gcode_filepath, "r")
 
-    def get_filename(self):
+    def get_filename(self) -> str:
         if self.__gcode_filepath:
             return self.__gcode_filepath.rsplit(os.sep, 1)[1]
         else:
             return "None selected"
 
-    def list_files(self):
+    def list_files(self) -> list[str]:
         return os.listdir(os.path.join(os.curdir, "uploads"))
 
-    def get_print_state(self):
+    def get_print_state(self) -> int:
         return self.__print_state
 
-    def get_print_state_name(self):
-        return self.__print_state.__qualname__
-
-    def set_print_state(self, state: int):
-        print(state)
+    def set_print_state(self, state: int) -> None:
         self.__print_state = state
 
-    def get_print_states(self):
+    def get_print_states(self) -> list[dict]:
         return [
-            {"name": "IDLE", "value": Printer.IDLE},
-            {"name": "PAUSED", "value": Printer.PAUSED},
-            {"name": "PRINTING", "value": Printer.PRINTING},
+            {"name": "IDLE", "value": IDLE},
+            {"name": "PAUSED", "value": PAUSED},
+            {"name": "PRINTING", "value": PRINTING},
         ]
-
-
-if __name__ == "__main__":
-    # printer = Printer()
-    # printer.connect()
-    # while True:
-    #     command = input("ender3 > ")
-    #     printer.queue_command(command)
-
-    pass
